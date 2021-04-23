e56315f390a3216ca64f8a1e649f805803b25ed5
Nuradin Sheikh <nuradin900@outlook.com>
2021-04-23 15:21:10 +0100

    Update

---
+++ src/Components/VideoSection.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../App.css'
+import { Button } from './Button';
+import './VideoSection.css';
+
+function VideoSection() {
+    return (
+        <div className='hero-container'>
+    <video src='' autoPlay loop muted/>
+    <h1>BRINGING FOOTBALL TO LIFE</h1>
+    <p>Live it, feel it, enjoy it</p>
+    <div className='hero-btns'>
+        <Button
+        className='btns'
+        buttonStyle='btn--outline'
+        buttonSize='btn--large'
+        >
+            GET STARTED
+        </Button>
+
+        <Button
+        className='btns'
+        buttonStyle='btn--primary'
+        buttonSize='btn--large'
+        >
+            WATCH TRAILER <i className='far fa-play-circle'/>
+        </Button>
+    </div>
+            
+        </div>
+    )
+}
+
+export default VideoSection
---
+++ src/Components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import VideoSection from '../VideoSection';
+
+
+ const Home = () => {
+    return(
+        <>
+       <VideoSection/>
+        </>
+    )
+}
+
+export default Home;
